183045dae93d60aef1eca8d7ec4889b1fe9d3ceb
chris <christianlfox@aol.com>
2023-11-20 21:18:55 +0000

    creation of .xaml code for gui

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -91,6 +91,37 @@ namespace RyanairFlightTrackBot
             EmailNotifier.NotifyMissingPriceBug();
         }
 
+
+        private void OnTextBoxClicked(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is TextBox textBox)
+            {
+                textBox.Text = string.Empty;
+                textBox.PreviewMouseLeftButtonDown -= OnTextBoxClicked; // Remove the event handler after the first click
+            }
+        }
+
+        private void TrackButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Retrieve values from text boxes
+            string departureAirport = DepartureAirportTextBox.Text;
+            string destinationAirport = DestinationAirportTextBox.Text;
+            string date = DateTextBox.Text;
+            string flightNumber = FlightNumberTextBox.Text;
+            List<string> emailList = new List<string>(NotifyEmailsTextBox.Text.Split(','));
+
+            // Create an instance of the Flight class
+            Flight newFlight = new Flight(departureAirport, destinationAirport, date, flightNumber, emailList);
+
+            // Now, you can use the 'newFlight' object as needed
+            // For example, you might want to perform further actions or store it in your application
+            // ...
+
+            // For now, let's display a message box with the flight details
+            MessageBox.Show($"New Flight Created:\n{newFlight.ToString()}", "Flight Tracking");
+        }
+
+
         public MainWindow()
         {
             InitializeComponent();
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # RyanairFlightTrackBot
+
+GitHub compatability!!
+ - Can sync (pull then push) whole project from VS2022. 
+    1. select Git from the top tab
+    2. select Sync
+    alternatively:
+    1. There is a Git Changes tab where solution explorer usually is. 
+    2. can pull and push from here
