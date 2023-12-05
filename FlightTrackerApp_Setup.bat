a032bb6f04b6eceaf1b93da4865ae46f9bee1d07
Christian Fox <74244335+christian-fox@users.noreply.github.com>
2023-12-05 11:32:24 +0000

    Create FlightTrackerApp_Setup.bat
    config file to store/set Environment Variables such as 
    SCHEDULED_TIME & GMAIL_APP_PASSWORD

---
+++ FlightTrackerApp_Setup.bat
@@ -0,0 +1,8 @@
+@echo off
+
+rem Set environment variables
+set SCHEDULED_TIME=2023-12-06T12:00:00
+set GMAIL_PASSWORD=mysecretpassword
+
+rem Run your C# application
+dotnet RyanAirFlightTrackBot.exe
