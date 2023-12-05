10551dab6019c1ef0ffa4493159be7a262f842e8
Christian Fox <74244335+christian-fox@users.noreply.github.com>
2023-12-05 11:36:30 +0000

    add ConfigReader class
    this class parses and reads the config file's env var's

---
+++ ConfigReader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace RyanairFlightTrackBot
+{
+
+    internal class AppConfig
+    {
+        internal DateTime NextScheduledTime { get; set; }
+        internal string GmailPassword { get; set; }
+    }
+
+
+    internal class ConfigReader
+    {
+        internal static AppConfig ReadConfig()
+        {
+            AppConfig config = new AppConfig();
+
+            try
+            {
+                // Read environment variables
+                string scheduledTimeString = Environment.GetEnvironmentVariable("SCHEDULED_TIME");
+                string gmailPassword = Environment.GetEnvironmentVariable("GMAIL_PASSWORD");
+
+                // Convert to DateTime if needed
+                config.NextScheduledTime = DateTime.Parse(scheduledTimeString);
+                config.GmailPassword = gmailPassword;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading environment variables: {ex.Message}");
+                // Handle the error as needed
+                //
+                // End programme ? Environment.Exit(0);  ???
+                //
+
+            }
+
+            return config;
+        }
+    }
